97883b8d71a9d0a77b198015ddf0901e4d65c551
Evan-Maddicks <evan.maddicks@mths.ca>
2024-10-16 13:54:30 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: October 16
+ * This program will show the distance of an object usin sonar.
 */
 
-basic.showString('Hello, World!')
+let distanceToObject:number = 0
+
+//setup
+basic.showIcon(IconNames.Heart)
+
+//find distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showString("cm")
+    basic.showIcon(IconNames.Happy)
+})
